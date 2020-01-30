8482cd8eb6ac73e619d1ed1d4775064ae8b11c8a
Adithya Pediredla <aditya.eee.nitw@gmail.com>
2020-01-30 12:04:42 -0500

    Added stricts option to avoid run time errors

--- renderer/samples/renderer3d_sample.cpp
+++ renderer/samples/renderer3d_sample.cpp
@@ -138,108 +138,187 @@ int main(int argc, char **argv) {
 	 */
 	std::string projectorTexture("/home/igkiou/Dropbox/AccoustoOptics+InvRendering/CodeEtc/SkeletalRenderer/ercrdr/renderer/images/White.pfm");
 
+	bool stricts=false;
+	bool bthreads=false;
+	bool bprecision=false;
+	bool bnumPhotons=false;
+	bool boutFilePrefix=false;
+	bool bsigmaT=false;
+	bool balbedo=false;
+	bool bgVal=false;
+	bool bf_u=false;
+	bool bspeed_u=false;
+	bool bn_o=false;
+	bool bn_max=false;
+	bool bmode=false;
+	bool ber_stepsize=false;
+	bool bdirectTol=false;
+	bool brrWeight=false;
+	bool bprojectorTexture=false;
+	bool buseDirect=false;
+	bool buseAngularSampling=false;
+	bool bmaxDepth=false;
+	bool bmaxPathlength=false;
+	bool bpathLengthMin=false;
+	bool bpathLengthMax=false;
+	bool bpathLengthBins=false;
+	bool bspatialX=false;
+	bool bspatialY=false;
+	bool bhalfThetaLimit=false;
+	bool bemitter_sensor_size=false;
+	bool bmediumLx=false;
+	bool bmediumRx=false;
+	bool bdistribution=false;
+	bool bgOrKappa=false;
+	bool blens_aperture=false;
+	bool blens_focalLength=false;
+	bool blens_active=false;
+	bool bprintInputs=false;
+
 	for(int i = 1; i < argc; i++){
 		std::vector<std::string> param = tokenize(argv[i], "=");
 		if(param.size() != 2){
 			std::cerr << "Input argument " << argv[i] << "should be in the format arg=value" << std::endl;
 			return -1;
 		}
-		if(param[0].compare("threads")==0)
-			threads = stoi(param[1]);
-		else if(param[0].compare("precision")==0)
-			precision = stoi(param[1]);
-		else if(param[0].compare("numPhotons")==0)
-			numPhotons = stoi(param[1]);
-		else if(param[0].compare("outFilePrefix")==0)
-			outFilePrefix = param[1];
-		else if(param[0].compare("sigmaT")==0)
-			sigmaT = stof(param[1]);
-		else if(param[0].compare("albedo")==0)
-			albedo = stof(param[1]);
-		else if(param[0].compare("gVal")==0)
-			gVal = stof(param[1]);
-		else if(param[0].compare("f_u")==0)
-			f_u = stof(param[1]);
-		else if(param[0].compare("speed_u")==0)
-			speed_u = stof(param[1]);
-		else if(param[0].compare("n_o")==0)
-			n_o = stof(param[1]);
-		else if(param[0].compare("n_max")==0)
-			n_max = stof(param[1]);
-		else if(param[0].compare("mode")==0)
-			mode = stoi(param[1]);
-		else if(param[0].compare("er_stepsize")==0)
-			er_stepsize = stof(param[1]);
-		else if(param[0].compare("directTol")==0)
-			directTol = stof(param[1]);
-		else if(param[0].compare("rrWeight")==0)
-			rrWeight = stof(param[1]);
-		else if(param[0].compare("projectorTexture")==0)
-			projectorTexture = param[1];
-		else if(param[0].compare("useDirect")==0){
+		if(param[0].compare("stricts")==0){ 
 			transform(param[1].begin(), param[1].end(), param[1].begin(), ::tolower);
-			if(param[1].compare("true")==0)
+			if(param[1].compare("true")==0) 
+				stricts = true;
+			else if(param[1].compare("false")==0) 
+				stricts = false;
+			else{
+				std::cerr << "stricts should be either true or false; Argument " << param[1] << " not recognized" << std::endl;
+				return -1;
+			}
+        }else if(param[0].compare("threads")==0){
+ 			bthreads=true;
+			threads = stoi(param[1]);
+		}else if(param[0].compare("precision")==0){
+ 			bprecision=true;
+			precision = stoi(param[1]);
+		}else if(param[0].compare("numPhotons")==0){
+ 			bnumPhotons=true;
+			numPhotons = stoi(param[1]);
+		}else if(param[0].compare("outFilePrefix")==0){
+ 			boutFilePrefix=true;
+			outFilePrefix = param[1];
+		}else if(param[0].compare("sigmaT")==0){
+ 			bsigmaT=true;
+			sigmaT = stof(param[1]);
+		}else if(param[0].compare("albedo")==0){
+ 			balbedo=true;
+			albedo = stof(param[1]);
+		}else if(param[0].compare("gVal")==0){
+ 			bgVal=true;
+			gVal = stof(param[1]);
+		}else if(param[0].compare("f_u")==0){
+ 			bf_u=true;
+			f_u = stof(param[1]);
+		}else if(param[0].compare("speed_u")==0){
+ 			bspeed_u=true;
+			speed_u = stof(param[1]);
+		}else if(param[0].compare("n_o")==0){
+ 			bn_o=true;
+			n_o = stof(param[1]);
+		}else if(param[0].compare("n_max")==0){
+ 			bn_max=true;
+			n_max = stof(param[1]);
+		}else if(param[0].compare("mode")==0){
+ 			bmode=true;
+			mode = stoi(param[1]);
+		}else if(param[0].compare("er_stepsize")==0){
+ 			ber_stepsize=true;
+			er_stepsize = stof(param[1]);
+		}else if(param[0].compare("directTol")==0){
+ 			bdirectTol=true;
+			directTol = stof(param[1]);
+		}else if(param[0].compare("rrWeight")==0){
+ 			brrWeight=true;
+			rrWeight = stof(param[1]);
+		}else if(param[0].compare("projectorTexture")==0){
+ 			bprojectorTexture=true;
+			projectorTexture = param[1];
+        }else if(param[0].compare("useDirect")==0){ 
+ 			buseDirect=true;
+			transform(param[1].begin(), param[1].end(), param[1].begin(), ::tolower);
+			if(param[1].compare("true")==0) 
 				useDirect = true;
-			else if(param[1].compare("false")==0)
+			else if(param[1].compare("false")==0) 
 				useDirect = false;
 			else{
 				std::cerr << "useDirect should be either true or false; Argument " << param[1] << " not recognized" << std::endl;
 				return -1;
 			}
-		}
-		else if(param[0].compare("useAngularSampling")==0){
+		}else if(param[0].compare("useAngularSampling")==0){
+ 			buseAngularSampling=true;
 			transform(param[1].begin(), param[1].end(), param[1].begin(), ::tolower);
-			if(param[1].compare("true")==0)
+			if(param[1].compare("true")==0) 
 				useAngularSampling = true;
-			else if(param[1].compare("false")==0)
+			else if(param[1].compare("false")==0) 
 				useAngularSampling = false;
 			else{
 				std::cerr << "useAngularSampling should be either true or false; Argument " << param[1] << " not recognized" << std::endl;
 				return -1;
 			}
-		}
-		else if(param[0].compare("maxDepth")==0)
+		}else if(param[0].compare("maxDepth")==0){
+ 			bmaxDepth=true;
 			maxDepth = stoi(param[1]);
-		else if(param[0].compare("maxPathlength")==0)
+		}else if(param[0].compare("maxPathlength")==0){
+ 			bmaxPathlength=true;
 			maxPathlength = stof(param[1]);
-		else if(param[0].compare("pathLengthMin")==0)
+		}else if(param[0].compare("pathLengthMin")==0){
+ 			bpathLengthMin=true;
 			pathLengthMin = stof(param[1]);
-		else if(param[0].compare("pathLengthMax")==0)
+		}else if(param[0].compare("pathLengthMax")==0){
+ 			bpathLengthMax=true;
 			pathLengthMax = stof(param[1]);
-		else if(param[0].compare("pathLengthBins")==0)
+		}else if(param[0].compare("pathLengthBins")==0){
+ 			bpathLengthBins=true;
 			pathLengthBins = stoi(param[1]);
-		else if(param[0].compare("spatialX")==0)
+		}else if(param[0].compare("spatialX")==0){
+ 			bspatialX=true;
 			spatialX = stoi(param[1]);
-		else if(param[0].compare("spatialY")==0)
+		}else if(param[0].compare("spatialY")==0){
+ 			bspatialY=true;
 			spatialY = stoi(param[1]);
-		else if(param[0].compare("halfThetaLimit")==0)
+		}else if(param[0].compare("halfThetaLimit")==0){
+ 			bhalfThetaLimit=true;
 			halfThetaLimit = stof(param[1]);
-		else if(param[0].compare("emitter_sensor_size")==0)
+		}else if(param[0].compare("emitter_sensor_size")==0){
+ 			bemitter_sensor_size=true;
 			emitter_sensor_size = stof(param[1]);
-		else if(param[0].compare("mediumLx")==0)
+		}else if(param[0].compare("mediumLx")==0){
+ 			bmediumLx=true;
 			mediumL[0] = stof(param[1]);
-		else if(param[0].compare("mediumRx")==0)
+		}else if(param[0].compare("mediumRx")==0){
+ 			bmediumRx=true;
 			mediumR[0] = stof(param[1]);
-		else if(param[0].compare("distribution")==0)
+		}else if(param[0].compare("distribution")==0){
+ 			bdistribution=true;
 			distribution = (param[1]);
-		else if(param[0].compare("gOrKappa")==0)
+		}else if(param[0].compare("gOrKappa")==0){
+ 			bgOrKappa=true;
 			gOrKappa = stof(param[1]);
-		else if(param[0].compare("lens_aperture")==0)
+		}else if(param[0].compare("lens_aperture")==0){
+ 			blens_aperture=true;
 			lens_aperture = stof(param[1]);
-		else if(param[0].compare("lens_focalLength")==0)
+		}else if(param[0].compare("lens_focalLength")==0){
+ 			blens_focalLength=true;
 			lens_focalLength = stof(param[1]);
-		else if(param[0].compare("lens_active")==0){
+        }else if(param[0].compare("lens_active")==0){ 
+ 			blens_active=true;
 			transform(param[1].begin(), param[1].end(), param[1].begin(), ::tolower);
-			if(param[1].compare("true")==0)
+			if(param[1].compare("true")==0) 
 				lens_active = true;
-			else if(param[1].compare("false")==0)
+			else if(param[1].compare("false")==0) 
 				lens_active = false;
 			else{
 				std::cerr << "lens_active should be either true or false; Argument " << param[1] << " not recognized" << std::endl;
 				return -1;
 			}
-		}
-		else if(param[0].compare("printInputs")==0){
+        }else if(param[0].compare("printInputs")==0){ 
+ 			bprintInputs=true;
 			transform(param[1].begin(), param[1].end(), param[1].begin(), ::tolower);
 			if(param[1].compare("true")==0)
 				printInputs = true;
@@ -249,11 +328,12 @@ int main(int argc, char **argv) {
 				std::cerr << "printInputs should be either true or false; Argument " << param[1] << " not recognized" << std::endl;
 				return -1;
 			}
-		}
-		else{
+		}else{
 			std::cerr << "Unknown variable in the input argument:" << param[0] << std::endl;
 			std::cerr << "Should be one of "
+	                  << "stricts, "
 					  << "threads, "
+					  << "precision, "
 					  << "numPhotons, "
 					  << "outFilePrefix, "
 					  << "sigmaT, "
@@ -270,15 +350,15 @@ int main(int argc, char **argv) {
 					  << "projectorTexture, "
 					  << "useDirect, "
 					  << "useAngularSampling, "
-					  << "maxDepth, " 
-					  << "maxPathlength, " 
-					  << "pathLengthMin, " 
-					  << "pathLengthMax, " 
-					  << "pathLengthBins, " 
+					  << "maxDepth, "
+					  << "maxPathlength, "
+					  << "pathLengthMin, "
+					  << "pathLengthMax, "
+					  << "pathLengthBins, "
 					  << "spatialX, "
 					  << "spatialY, "
-                      << "halfThetaLimit, "
-                      << "emitter_sensor_size, "
+					  << "halfThetaLimit, "
+					  << "emitter_sensor_size, "
 					  << "mediumLx, "
 					  << "mediumRx, "
 					  << "distribution, "
@@ -291,6 +371,49 @@ int main(int argc, char **argv) {
             return -1;
         }
 	}
+
+    if(stricts){
+		if(!bthreads) {std::cout << "threads is not specified " << std::endl;}
+		if(!bprecision) {std::cout << "precision is not specified " << std::endl;}
+		if(!bnumPhotons) {std::cout << "numPhotons is not specified " << std::endl;}
+		if(!boutFilePrefix) {std::cout << "outFilePrefix is not specified " << std::endl;}
+		if(!bsigmaT) {std::cout << "sigmaT is not specified " << std::endl;}
+		if(!balbedo) {std::cout << "albedo is not specified " << std::endl;}
+		if(!bgVal) {std::cout << "gVal is not specified " << std::endl;}
+		if(!bf_u) {std::cout << "f_u is not specified " << std::endl;}
+		if(!bspeed_u) {std::cout << "speed_u is not specified " << std::endl;}
+		if(!bn_o) {std::cout << "n_o is not specified " << std::endl;}
+		if(!bn_max) {std::cout << "n_max is not specified " << std::endl;}
+		if(!bmode) {std::cout << "mode is not specified " << std::endl;}
+		if(!ber_stepsize) {std::cout << "er_stepsize is not specified " << std::endl;}
+		if(!bdirectTol) {std::cout << "directTol is not specified " << std::endl;}
+		if(!brrWeight) {std::cout << "rrWeight is not specified " << std::endl;}
+		if(!bprojectorTexture) {std::cout << "projectorTexture is not specified " << std::endl;}
+		if(!buseDirect) {std::cout << "useDirect is not specified " << std::endl;}
+		if(!buseAngularSampling) {std::cout << "useAngularSampling is not specified " << std::endl;}
+		if(!bmaxDepth) {std::cout << "maxDepth is not specified " << std::endl;}
+		if(!bmaxPathlength) {std::cout << "maxPathlength is not specified " << std::endl;}
+		if(!bpathLengthMin) {std::cout << "pathLengthMin is not specified " << std::endl;}
+		if(!bpathLengthMax) {std::cout << "pathLengthMax is not specified " << std::endl;}
+		if(!bpathLengthBins) {std::cout << "pathLengthBins is not specified " << std::endl;}
+		if(!bspatialX) {std::cout << "spatialX is not specified " << std::endl;}
+		if(!bspatialY) {std::cout << "spatialY is not specified " << std::endl;}
+		if(!bhalfThetaLimit) {std::cout << "halfThetaLimit is not specified " << std::endl;}
+		if(!bemitter_sensor_size) {std::cout << "emitter_sensor_size is not specified " << std::endl;}
+		if(!bmediumLx) {std::cout << "mediumLx is not specified " << std::endl;}
+		if(!bmediumRx) {std::cout << "mediumRx is not specified " << std::endl;}
+		if(!bdistribution) {std::cout << "distribution is not specified " << std::endl;}
+		if(!bgOrKappa) {std::cout << "gOrKappa is not specified " << std::endl;}
+		if(!blens_aperture) {std::cout << "lens_aperture is not specified " << std::endl;}
+		if(!blens_focalLength) {std::cout << "lens_focalLength is not specified " << std::endl;}
+		if(!blens_active) {std::cout << "lens_active is not specified " << std::endl;}
+		if(!bprintInputs) {std::cout << "printInputs is not specified " << std::endl;}
+
+        if(!(bthreads && bprecision && bnumPhotons && boutFilePrefix && bsigmaT && balbedo && bgVal && bf_u && bspeed_u && bn_o && bn_max && bmode && ber_stepsize && bdirectTol && brrWeight && bprojectorTexture && buseDirect && buseAngularSampling && bmaxDepth && bmaxPathlength && bpathLengthMin && bpathLengthMax && bpathLengthBins && bspatialX && bspatialY && bhalfThetaLimit && bemitter_sensor_size && bmediumLx && bmediumRx && bdistribution && bgOrKappa && blens_aperture && blens_focalLength && blens_active && bprintInputs)){
+            std::cout << "crashing as one or more inputs is absent" << std::endl;
+            exit (EXIT_FAILURE);
+        }
+    }
 
 	if(printInputs){
 		std::cout << "numPhotons = "<< numPhotons 	<< std::endl;
