ec03f5ea06dfa9c4b3d894daf968b86d169007a1
Adithya Pediredla <apedired@andrew.cmu.edu>
2020-08-06 14:03:24 +0530

    Make wrapSrinivas code parallel and with less RAM footprint to run it on cluster.

--- renderer/pybindFiles/tests/wrapSrinivasData.py
+++ renderer/pybindFiles/tests/wrapSrinivasData.py
@@ -10,12 +10,32 @@ this_dir = str(os.path.dirname(os.path.abspath(__file__)))
 sys.path.append(this_dir + '/../bin')
 from random import seed
 from random import random
+from functools import partial
+
 
 from multiprocessing import Pool, TimeoutError
 import image_pybind
 
+def readAllFrequencies(nSearches, n_max_min, n_max_max, f_u_min, f_u_max, MFPs, s, folderPrefix, Mask, v_n):
+    Intensities = np.zeros((nSearches))
+    for f_n in range(0, nSearches):
+        n_max = round(n_max_min + v_n*(n_max_max - n_max_min)/nSearches, 4)
+        f_u = round(f_u_min + f_n*(f_u_max - f_u_min)/nSearches,1)
+        outFileName = folderPrefix + "MFPs_" + MFPs[s] + "_n_max_" + str(n_max) + "_f_u_" + str(f_u) + ".pfm3d"
+        if not os.path.exists(outFileName):
+            print(outFileName + " does not exist")
+            exit()
+        Image = image_pybind.SmallImage(10,10,10)
+        Image.readPFM3D(outFileName)
+        temp = 0.0
+        for i in range(0, 1023):
+            for j in range(0, 1023):
+                temp += Mask[i,j] * Image.getPixel(i, j, 0)
+        Intensities[f_n] = temp;
+    return Intensities
+
 #fileName = '/home/apedired/Adi2TB/Dropbox/AccoustoOptics+InvRendering/CodeEtc/SkeletalRenderer/ercrdr_angletracingNEE/renderer/images/Disk_501.pfm'
-p = Pool(72)
+pool = Pool()
 folderPrefix = '/shared/LSA/VF/1M/ER_'
 
 MFPs = ["0"];
@@ -43,25 +63,10 @@ for i in range(0, 1023):
 Intensities = np.zeros((len(MFPs), nSearches, nSearches))
 
 for s in range(0, len(MFPs)):
-    for v_n in range(0, nSearches):
-        start = time.time()
-        pool.map(f, range(10))
-        for f_n in range(0, nSearches):
-            n_max = round(n_max_min + v_n*(n_max_max - n_max_min)/nSearches, 4)
-            f_u = round(f_u_min + f_n*(f_u_max - f_u_min)/nSearches,1)
-            outFileName = folderPrefix + "MFPs_" + MFPs[s] + "_n_max_" + str(n_max) + "_f_u_" + str(f_u) + ".pfm3d"
-            if not os.path.exists(outFileName):
-                print(outFileName + " does not exist")
-                exit()
-            Image = image_pybind.SmallImage(10,10,10)
-            Image.readPFM3D(outFileName)
-            temp = random()
-#            for i in range(0, 1023):
-#                for j in range(0, 1023):
-#                    temp += Mask[i,j] * Image.getPixel(i, j, 0)
-            Intensities[s, v_n, f_n] = temp;
-        end = time.time()
-        print(end - start)
+    func = partial(readAllFrequencies, nSearches, n_max_min, n_max_max, f_u_min, f_u_max, MFPs, s, folderPrefix, Mask)
+    for v_n, res in enumerate(pool.imap(func, range(0, nSearches)), 0):
+        Intensities[s, v_n, :] = res
+
 
 
 m_dic = {"Intensities": Intensities, "label": "Radius_0.1"}
