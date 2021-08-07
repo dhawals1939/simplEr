ec91db409055c55d9237426ed557201966780f3d
Andre <cnasc.andre@gmail.com>
2021-08-07 12:47:38 -0400

    Add random number generation to cuda renderer

--- renderer/include/cudaRenderer.h
+++ renderer/include/cudaRenderer.h
@@ -1,22 +1,48 @@
+/*
+ * cudaRenderer.h
+ *
+ *  Created on: Aug 2, 2021
+ *      Author: Andre
+ */
+
+
+#ifndef CUDA_RENDERER_H_
+#define CUDA_RENDERER_H_
+
 #include "image.h"
+#include "curand.h"
+
+
+typedef unsigned int CudaSeedType;
 
 class CudaRenderer {
 
 public:
 
-    CudaRenderer();
+    CudaRenderer(image::SmallImage& img, int numPhotons)
+        : img(img), \
+          numPhotons(numPhotons){}
+
     virtual ~CudaRenderer();
 
-    void renderImage(image::SmallImage& img, int numPhotons);
+    void renderImage();
 
 private:
 
-    void setup(image::SmallImage& img);
+    void setup();
+    float *genDeviceRandomNumbers(CudaSeedType seed);
+
+    image::SmallImage& img;
+    int numPhotons;
+
 
     /* Host memory*/
     float *image;
 
     /* Device memory*/
     float *cudaImage;
+    float *cudaRandom;
 
 };
+
+#endif /* CUDA_RENDERER_H_ */
--- renderer/src/cudaRenderer.cu
+++ renderer/src/cudaRenderer.cu
@@ -1,5 +1,11 @@
+/*
+ * cudaRenderer.cu
+ *
+ *  Created on: Aug 2, 2021
+ *      Author: Andre
+ */
+
 #include "cudaRenderer.h"
-#include "image.h"
 
 /* TODO:
  * - The contribution of a photon equates to a sum on the pixel(s) it affects. Figure
@@ -7,10 +13,18 @@
  * - Random number generation has to match that of the boost library. In the future,
  * support for the sse rng as well. */
 
+#define CUDA_CALL(x) do { if((x)!=cudaSuccess) { \
+    printf("Error at %s:%d\n",__FILE__,__LINE__);\
+    return EXIT_FAILURE;}} while(0)
+#define CURAND_CALL(x) do { if((x)!=CURAND_STATUS_SUCCESS) { \
+    printf("Error at %s:%d\n",__FILE__,__LINE__);\
+    return EXIT_FAILURE;}} while(0)
+
 // Store symbols here so as to avoid long argument list
 // in kernel calls
 struct Constants {
     float* image;
+    float* random;
 };
 
 __constant__ Constants constants;
@@ -25,20 +39,37 @@ void CudaRenderer::renderImage(image::SmallImage& img, int numPhotons) {
     setup(img);
 
     renderPhotons<<<1,numPhotons>>>();
-    cudaDeviceSynchronize();
+    CUDA_CALL(cudaDeviceSynchronize());
 
 }
 
-void CudaRenderer::setup(image::SmallImage& img) {
+/* Allocates device data, sends parameters to device and sets up RNG. */
+void CudaRenderer::setup(image::SmallImage& img, int numPhotons) {
+    CUDA_CALL(cudaMalloc((void **)&cudaImage,
+                         img.getXRes()*img.getYRes()*img.getZRes()*sizeof(float)));
 
-    // use smallImage.x, y and z
-    cudaMalloc(&cudaImage, sizeof(float) * img.getXRes() * img.getYRes() * img.getZRes());
+    genDeviceRandomNumbers();
 
+    /* Send in parameters to device */
     Constants params;
     params.image = cudaImage;
+    params.random = cudaRandom;
 
-    cudaMemcpyToSymbol(constants, &params, sizeof(Constants));
+    CUDA_CALL(cudaMemcpyToSymbol(constants, &params, sizeof(Constants)));
 
+}
+
+// TODO: Ensure ordering and offset are the same as boost
+/* Generates random numbers on the device. */
+void CudaRenderer::genDeviceRandomNumbers(CudaSeedType seed = CudaSeedType(5489)) {
+    int num = requiredRandomNumbers(numPhotons);
+    CUDA_CALL(cudaMalloc((void **)&cudaRandom, num * sizeof(float)));
+
+    CURAND_CALL(curandCreateGenerator(&generator, CURAND_RNG_PSEUDO_MT19937));
+    CURAND_CALL(curandSetPseudoRandomGeneratorSeed(generator, seed));
+
+    /* Generate reals uniformly between 0.0 and 1.0 */
+    CURAND_CALL(curandGenerateUniform(generator, cudaRandom, num));
 }
 
 CudaRenderer::~CudaRenderer() {
@@ -48,8 +79,16 @@ CudaRenderer::~CudaRenderer() {
     }
 
     if (cudaImage) {
-        cudaFree(cudaImage);
+        CURAND_CALL(curandDestroyGenerator(generator));
+
+        CUDA_CALL(cudaFree(cudaImage));
+        CUDA_CALL(cudaFree(cudaRandom));
     }
 
     // No need to free constant memory
+}
+
+/* Required amount of random numbers to run the renderPhotons kernel on numPhotons */
+unsigned int requiredRandomNumbers(unsigned int numPhotons) {
+    return numPhotons * 4;
 }
