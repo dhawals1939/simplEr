081942402b4a8fe7306bc3277474c38090f1d404
Andre <cnasc.andre@gmail.com>
2021-10-14 17:08:42 -0400

    Move implementation to single cuda_renderer.cu file

--- renderer/src/cuda_renderer.cu
+++ renderer/src/cuda_renderer.cu
@@ -8,6 +8,7 @@
 #include "cuda_renderer.h"
 #include "cuda_vector.cuh"
 #include "cuda_utils.cuh"
+#include "cuda_scene.cuh"
 
 namespace cuda {
 
@@ -22,6 +23,43 @@ struct Constants {
 };
 
 __constant__ Constants d_constants;
+
+__device__ Float Sampler::sample(short &uses) const{
+    ASSERT(uses < RANDOM_NUMBERS_PER_PHOTON);
+    ASSERT(threadIdx.x * RANDOM_NUMBERS_PER_PHOTON + uses + 1 < m_size);
+    return m_random[threadIdx.x * RANDOM_NUMBERS_PER_PHOTON + uses++];
+}
+
+// Sample random ray
+__device__ bool AreaTexturedSource::sampleRay(TVector3<Float> &pos, TVector3<Float> &dir,
+                                      Float &totalDistance, Sampler *sampler, short &samplerUses) const{
+    pos = *m_origin;
+
+    // sample pixel position first
+	int pixel = m_textureSampler->sample(sampler->sample(samplerUses));
+	int p[2];
+	m_texture->ind2sub(pixel, p[0], p[1]);
+
+	// Now find a random location on the pixel
+	for (int iter = 1; iter < m_origin->dim; ++iter) {
+		pos[iter] += - (*m_plane)[iter - 1] / FPCONST(2.0) +
+            p[iter - 1] * (*m_pixelsize)[iter-1] + sampler->sample(samplerUses) * (*m_pixelsize)[iter - 1];
+	}
+
+	dir = *m_dir;
+
+	//FIXME: Hack: Works only for m_dir = [-1 0 0]
+	Float z   = sampler->sample(samplerUses)*(1-m_ct) + m_ct;
+	Float zt  = sqrtf(FPCONST(1.0)-z*z);
+    // FIXME: FPCONST(M_PI) might be generating complaints here
+	Float phi = sampler->sample(samplerUses)*2*M_PI;
+    // FIXME: operator[] overload here might be causing issues
+	dir[0] = -z;
+	dir[1] = zt*cosf(phi);
+	dir[2] = zt*sinf(phi);
+
+	return propagateTillMedium(pos, dir, totalDistance);
+}
 
 __device__ void scatter() {
 //	Assert(scene.getMediumBlock().inside(p));
