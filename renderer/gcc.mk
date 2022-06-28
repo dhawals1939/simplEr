94c7c52705249e28e8d7aca226857536e2728e70
Adithya Pediredla <apedired@andrew.cmu.edu>
2022-06-27 22:32:15 -0400

    DEBUG_MODE fix

--- renderer/gcc.mk
+++ renderer/gcc.mk
@@ -1,7 +1,11 @@
 CC = g++
 #GENERALFLAGS = -fPIC -W -Wall -Wextra -g -pedantic -std=c++14
 GENERALFLAGS = -fPIC -std=c++14
-OPTIMFLAGS = -march=native -O3 -ffast-math -fopenmp -pthread '-pipe' '-march=native' '-msse2' '-ftree-vectorize' '-mfpmath=sse' '-funsafe-math-optimizations' '-fno-rounding-math' '-fno-signaling-nans' '-fno-math-errno' '-fomit-frame-pointer'
+ifeq ($(DEBUG_MODE), 1)
+    OPTIMFLAGS = -march=native -O0 -ffast-math -fopenmp -pthread '-pipe' '-march=native' '-msse2' '-ftree-vectorize' '-mfpmath=sse' '-funsafe-math-optimizations' '-fno-rounding-math' '-fno-signaling-nans' '-fno-math-errno' '-fomit-frame-pointer'
+else
+    OPTIMFLAGS = -march=native -O3 -ffast-math -fopenmp -pthread '-pipe' '-march=native' '-msse2' '-ftree-vectorize' '-mfpmath=sse' '-funsafe-math-optimizations' '-fno-rounding-math' '-fno-signaling-nans' '-fno-math-errno' '-fomit-frame-pointer'
+endif
 #REPORTSFLAGS = -Winline -Wimplicit
 REPORTSFLAGS =
 #DEBUGFLAG = -g
