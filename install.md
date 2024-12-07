82fa84e76bd6e84d45bd7c6018766ba1a5a0f6e5
Dhawal S <dhawal.sirikonda.gr@dartmouth.edu>
2024-12-07 18:16:37 -0500

    adding some package manager

---
+++ install.md
@@ -0,0 +1,18 @@
+# Installation Instructions
+
+Install [!vcpkg](https://github.com/microsoft/vcpkg.git)
+Install [!cmake](https://cmake.org/)
+
+Set the `VCPKG_ROOT` to the directory with `vcpkg` binary. Also add this director to PATH.
+
+## Windows
+
+In windows machines:
+
+`cmake --preset windows`
+
+## Linux
+
+In linux machines
+
+`cmake --preset linux`
--- simplER.cpp
+++ simplER.cpp
@@ -1,7 +1,26 @@
 #include <fmt/core.h>
+#include <boost/algorithm/string.hpp>
+#include <Eigen/Dense>
+#include <iostream>
 
 int main()
 {
+    // Test fmt
     fmt::print("Hello World!\n");
+
+    // Test Boost
+    std::string s = "Boost Library";
+    boost::to_upper(s);
+    fmt::print("Boost: {}\n", s);
+
+    // Test Eigen3
+    Eigen::MatrixXd mat(2, 2);
+    mat(0, 0) = 3;
+    mat(1, 0) = 2.5;
+    mat(0, 1) = -1;
+    mat(1, 1) = mat(1, 0) + mat(0, 1);
+    std::cout << "Eigen3 Matrix:\n"
+              << mat << std::endl;
+
     return 0;
 }
